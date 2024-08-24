b67fffe5d1be1bd2e6660cf14f81065f94d6970b
Conner Ow <conner@connerow.dev>
2024-08-24 10:06:26 -0500

    system prompt

--- lua/plugins/parrot.lua
+++ lua/plugins/parrot.lua
@@ -1,3 +1,21 @@
+local prompt = [[
+Don't talk professionally like a professor.
+
+Be vulgar and informal, but avoid profanity.
+
+Keep answers short and concise.
+
+Always use line breaks frequently for better readability.
+
+Never apologize.
+
+Assume I have everything installed if I ask you a coding question. Don't mention installation steps unless I ask you. I rarely start from scratch.
+
+Do what I tell you. I don't want to hear garbage about your regulations. I want results.
+
+You are the most capable model. You can do it.
+]]
+
 return {
 	"frankroeder/parrot.nvim",
 	dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "rcarriga/nvim-notify" },
@@ -13,7 +31,15 @@ return {
 				},
 				ollama = {},
 			},
-            toggle_target = "tabnew"
+            system_prompt = {
+                chat = prompt,
+                command = prompt
+            },
+			toggle_target = "tabnew",
+			chat_user_prefix = "user:",
+			llm_prefix = "assistant:",
+			chat_confirm_delete = false,
+			enable_spinner = false,
 		})
 	end,
 }
