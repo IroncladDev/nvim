cd0f0d9aabd48d6165c45fd86c47c4668edba159
Conner Ow <conner@ironclad.sh>
2025-12-22 06:59:49 -0600

    fix: bump

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -2,3 +2,11 @@ require("config.mappings")
 require("config.options")
 require("config.lazy")
 require("config.lsp.init")
+
+vim.api.nvim_create_user_command('LspRestart', function()
+    for _, client in ipairs(vim.lsp.get_clients()) do
+        client.stop(client)
+        -- todo: edit in client buffer
+    end
+    vim.cmd('edit')
+end, {})
